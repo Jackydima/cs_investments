ef6e1500802e7353a9ca26a3919ab4c83f65b11a
Dimitri <dzmitry.brusilouski@hs-augsburg.de>
2023-08-06 19:40:45 +0200

    Write a csv File for Importing into sheets

--- main.js
+++ main.js
@@ -1,27 +1,39 @@
 import { getCSGOMarketData, formatPrice } from './getCSGOMarketData.js';
 import getJson from './getJson.js';
+import { writeFileSync } from 'fs'
 
 const timer = delay => new Promise(res => setTimeout(res, delay));
+/*
+writeFileSync('./data.csv', "Item, Price\n", err => {
+    if (err) {
+        console.log("Error: ", err)
+        return -1
+    }
+})
+*/
 
 const getPrices = () => {
-
     getJson("http://localhost:4000/cases")
         .then(async (value) => {
             for (const item in value) {
                 getCSGOMarketData(value[item]["url"], "lowest_price").then((value => {
                     const price = formatPrice(value);
                     console.log(`${item}, ${price}`);
+                    writeFileSync('./data.csv', `${item}, ${price}\n`, { flag: "a" }, err => {
+                        if (err) {
+                            console.log("Error: ", err)
+                            return -1
+                        }
+                    })
                 }))
-                .catch((error) => { throw error } );
-                await timer(100);
+                    .catch((error) => { throw error });
+                await timer(1000);
             }
-        }
-        )
+        })
         .catch((error) => {
             console.log(`Error in getPrices:\n ${error.message}`);
             throw error;
         });
-
 }
 
 try {
